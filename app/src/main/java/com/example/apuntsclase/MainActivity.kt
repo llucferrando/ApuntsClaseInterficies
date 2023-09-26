a367290e4c02e20c760858aad62096b7df8d6080
llucferrando <lluc.ferrando@enti.cat>
2023-09-26 10:39:48 +0200

    Starting UI

---
+++ app/src/main/java/com/example/apuntsclase/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.apuntsclase
+
+import android.os.Bundle
+import android.provider.CalendarContract.Colors
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.apuntsclase.ui.theme.ApuntsClaseTheme
+
+class MainActivity : ComponentActivity() {
+    var counter = 0;
+
+    val text: TextView by lazy { findViewById(R.id.start_screen_text) }
+    val button: TextView by lazy { findViewById(R.id.start_screen_button) }
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.start_screen)
+
+
+
+        button.setOnClickListener{
+            counter++
+            text.text = counter.toString()
+        }
+    }
+
+}
+
+
