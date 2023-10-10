595ee592cb6a5d41296492f31f4ad3b4f25abd46
llucferrando <lluc.ferrando@enti.cat>
2023-10-10 11:05:28 +0200

    Calculator with few errors

---
+++ app/src/main/java/com/example/apuntsclase/ButtonGrid.kt
@@ -0,0 +1,56 @@
+package com.example.apuntsclase;
+
+import android.content.Context
+import android.widget.Button
+import android.widget.LinearLayout;
+
+public class ButtonGrid(val context: Context, val parent: LinearLayout) {
+    class ButtonsRow (val context: Context,val parent: LinearLayout){
+        var buttons: MutableList<Button> = mutableListOf()
+        private val _linearLayout: LinearLayout = LinearLayout(context)
+        init{
+            _linearLayout.orientation = LinearLayout.HORIZONTAL
+            _linearLayout.layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                1.0f
+
+            )
+            _linearLayout.setBackgroundColor(context.getColor(R.color.teal_200))
+            parent.addView(_linearLayout)
+        }
+        fun AddNewButton(text:String) : Button {
+            var _bt = Button(context)
+            _bt.text = text
+            _bt.layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                1.0f
+            )
+            _linearLayout.addView(_bt)
+            buttons.add(_bt)
+
+            return _bt
+        }
+
+
+    }
+
+    private var rows: MutableList<ButtonsRow> = mutableListOf()
+    fun AddNewRow() : ButtonsRow {
+        val btRow = ButtonsRow(context,parent)
+        rows.add(btRow)
+
+        return btRow
+    }
+
+    fun GetRow(index: Int): ButtonsRow? {
+        if (index >= rows.count() || index < 0) {
+            return null
+
+        }
+
+        return rows[index]
+    }
+
+}
--- app/src/main/java/com/example/apuntsclase/MainActivity.kt
+++ app/src/main/java/com/example/apuntsclase/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.apuntsclase
 import android.os.Bundle
 import android.provider.CalendarContract.Colors
 import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -20,15 +21,99 @@ import androidx.core.widget.doBeforeTextChanged
 import com.example.apuntsclase.ui.theme.ApuntsClaseTheme
 
 class MainActivity : ComponentActivity() {
+    val result: TextView by lazy { findViewById(R.id.result) }
+    val bContainer: LinearLayout by lazy{findViewById(R.id.buttons_container)}
+    enum class Operation(val operation:(a:Int, b:Int)->Int, val char: String){
+        Add({a,b->a + b}, " + "),
+        Substract({a,b-> a - b}, " - "),
+        Multiply({a,b-> a * b}, " * "),
+        Divide({a,b-> a / b}, " / ");
 
+        public fun Operate(a:Int, b:Int): Int{
+            return this.operation(a,b);
+        }
+    }
 
-val reuslt: TextView by lazy { findViewById(R.id.result) }
+    var A:Int? = null
+    var B:Int? = null
+    var Op:Operation?=null
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.start_screen)
 
 
+        val buttonGrid = ButtonGrid(this,bContainer)
 
+        var names: MutableList<MutableList<String>> = mutableListOf()
+        names.add(mutableListOf("AC", "()","%","/"))
+        names.add(mutableListOf("7", "8","9","X"))
+        names.add(mutableListOf("4", "5","6","-"))
+        names.add(mutableListOf("1", "2","3","+"))
+        names.add(mutableListOf("0", ".","<-","="))
+        for(y in names.indices){
+            val row =  buttonGrid.AddNewRow()
+            for(x in names[y].indices){
+                val _bt = row.AddNewButton(names[y][x])
+                _bt.setOnClickListener{
+                    when(names[x][y]){
+                        "AC"->{
+                            A = null
+                            B = null
+                            Op = null
+                            result.text = "0"
+                        }
+                        "0","1","2","3","4","5","6","7","8","9"->{
+                          NumberPress(names[x][y].toInt())
+                            "+" ->{
+                                OperationPress(Operation.Add)
+                            }
+                            "-" ->{
+                                OperationPress(Operation.Substract)
+                            }
+                            "*" ->{
+                                OperationPress(Operation.Multiply)
+                            }
+                            "/" ->{
+                                OperationPress(Operation.Divide)
+                            }
+                            "=" ->{
+                               A?.let{a->
+                                   B?.let{b->
+                                       val resultNum
+                                   }
+                               }
+                            }
+                        }
+
+                    }
+                }
+
+            }
+        }
+    }
+    fun NumberPress(num:Int){
+        if(Op==null){
+            A?.let{ a->
+                A = (a*10) + num
+            } ?: run{
+                A=num
+            }
+            result.text=A.toString()
+        }else{
+            B?.let{ b->
+                B = (b*10) + num
+            } ?: run{
+                B=num
+            }
+
+            result.text = A.toString() + Op.toString() + B.toString()
+        }
+    }
+    fun OperationPress(op:Operation){
+        if(A!=null && B==null) {
+            Op = op
+            result.text = A.toString() + Op?.char
+        }
     }
 
 }
